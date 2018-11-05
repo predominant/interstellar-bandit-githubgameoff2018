3bbac7195a043b8b50484dd98e9cf3d9034d134d
Graham Weldon <graham@grahamweldon.com>
2018-11-05 23:48:14 +0900

    Cars now follow train engine
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- Assets/ghg2018/Scripts/TrainCar.cs
+++ Assets/ghg2018/Scripts/TrainCar.cs
@@ -4,6 +4,49 @@ namespace ghg2018
 {
     public class TrainCar : MonoBehaviour
     {
+        [SerializeField]
+        private TrainCar _leadCar;
+
+        [SerializeField]
+        private float _leadCarSpacing = 0.1f;
         
+        [SerializeField]
+        private TrainRail _trainRail;
+
+        [SerializeField]
+        private float Speed = 1f;
+
+        [HideInInspector]
+        public float TrackPosition = 0f;
+
+        private void FixedUpdate()
+        {
+            this.UpdateTrackPosition();
+        }
+        
+        private void Update()
+        {
+            this.transform.position = this._trainRail.PositionByRatio(this.TrackPosition);
+            this.transform.rotation = Quaternion.LookRotation(this._trainRail.TangentByRatio(this.TrackPosition));
+        }
+
+        private void UpdateTrackPosition()
+        {
+            if (this._leadCar == null)
+            {
+                this.TrackPosition += this.Speed * Time.fixedDeltaTime;
+            }
+            else
+            {
+                this.TrackPosition = this._leadCar.TrackPosition - this._leadCarSpacing;
+            }
+
+            // TODO: Replace wrap around
+            // Wrap around on the current track
+            if (this.TrackPosition < 0f)
+                this.TrackPosition += 1f;
+            if (this.TrackPosition >= 1f)
+                this.TrackPosition -= 1f;
+        }
     }
 }
--- Assets/ghg2018/Scripts/TrainEngine.cs
+++ Assets/ghg2018/Scripts/TrainEngine.cs
@@ -4,30 +4,7 @@ using UnityEngine;
 
 namespace ghg2018
 {
-	public class TrainEngine : MonoBehaviour
+	public class TrainEngine : TrainCar
 	{
-		[SerializeField]
-		private TrainCar _trainCar;
-
-		[SerializeField]
-		private TrainRail _trainRail;
-
-		[SerializeField]
-		private float Speed = 1f;
-
-		private float _trackPosition = 0f;
-
-		private void FixedUpdate()
-		{
-			this._trackPosition += this.Speed * Time.fixedDeltaTime;
-			if (this._trackPosition >= 1f)
-				this._trackPosition -= 1f;
-		}
-
-		private void Update()
-		{
-			this.transform.position = this._trainRail.PositionByRatio(this._trackPosition);
-			this.transform.rotation = Quaternion.LookRotation(this._trainRail.TangentByRatio(this._trackPosition));
-		}
 	}
 }
